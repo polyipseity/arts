5e943f6fad8d16cbd850fa86e5fd3543fbf64363
William So <polyipseity@gmail.com>
2023-09-08 22:21:44 +0800

    Create scripts `split` and `unsplit`
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ scripts/split.py
@@ -0,0 +1,97 @@
+# -*- coding: UTF-8 -*-
+from anyio import Path as _Path, open_file as _open_f
+from argparse import (
+    ArgumentParser as _ArgParser,
+    Namespace as _NS,
+    ONE_OR_MORE as _ONE_OR_MORE,
+)
+from asyncio import gather as _gather, run as _run
+from asyncstdlib import enumerate as _aenumerate
+from dataclasses import dataclass as _dc
+from functools import partial as _partial, wraps as _wraps
+from logging import INFO as _INFO, basicConfig as _basicConfig
+from sys import argv as _argv, modules as _mods
+from typing import Callable as _Call, Sequence as _Seq, final as _fin
+
+_SPLIT_SIZE = 10 * 1024**2  # 10 MiB
+
+
+@_fin
+@_dc(
+    init=True,
+    repr=True,
+    eq=True,
+    order=False,
+    unsafe_hash=False,
+    frozen=True,
+    match_args=True,
+    kw_only=True,
+    slots=True,
+)
+class Arguments:
+    inputs: _Seq[_Path]
+
+    def __post_init__(self):
+        object.__setattr__(self, "inputs", tuple(self.inputs))
+
+
+async def main(args: Arguments):
+    async def split(path: _Path):
+        async with await path.open(mode="rb") as file:
+
+            async def chunks():
+                chunk = await file.read(_SPLIT_SIZE)
+                while chunk:
+                    yield chunk
+                    chunk = await file.read(_SPLIT_SIZE)
+
+            async for count, chunk in _aenumerate(chunks(), 1):
+                async with await _open_f(
+                    "{}.{:03}".format(path, count), mode="wb"
+                ) as split:
+                    await split.write(chunk)
+
+    await _gather(*map(split, args.inputs))
+
+
+def parser(parent: _Call[..., _ArgParser] | None = None):
+    prog0 = _mods[__name__].__package__
+    prog = prog0 if prog0 else __name__
+    del prog0
+
+    parser = (_ArgParser if parent is None else parent)(
+        prog=f"python -m {prog}",
+        description="split file(s)",
+        add_help=True,
+        allow_abbrev=False,
+        exit_on_error=False,
+    )
+    parser.add_argument(
+        "inputs",
+        action="store",
+        nargs=_ONE_OR_MORE,
+        type=_Path,
+        help="sequence of input(s) to read",
+    )
+
+    @_wraps(main)
+    async def invoke(args: _NS):
+        await main(
+            Arguments(
+                inputs=await _gather(
+                    *map(
+                        _partial(_Path.resolve, strict=True),
+                        args.inputs,
+                    )
+                ),
+            )
+        )
+
+    parser.set_defaults(invoke=invoke)
+    return parser
+
+
+if __name__ == "__main__":
+    _basicConfig(level=_INFO)
+    entry = parser().parse_args(_argv[1:])
+    _run(entry.invoke(entry))
---
+++ scripts/unsplit.py
@@ -0,0 +1,86 @@
+# -*- coding: UTF-8 -*-
+from anyio import Path as _Path
+from argparse import (
+    ArgumentParser as _ArgParser,
+    Namespace as _NS,
+    ONE_OR_MORE as _ONE_OR_MORE,
+)
+from asyncio import gather as _gather, run as _run
+from dataclasses import dataclass as _dc
+from functools import wraps as _wraps
+from itertools import cycle as _cycle
+from logging import INFO as _INFO, basicConfig as _basicConfig
+from sys import argv as _argv, modules as _mods
+from typing import Callable as _Call, Sequence as _Seq, final as _fin
+
+
+@_fin
+@_dc(
+    init=True,
+    repr=True,
+    eq=True,
+    order=False,
+    unsafe_hash=False,
+    frozen=True,
+    match_args=True,
+    kw_only=True,
+    slots=True,
+)
+class Arguments:
+    inputs: _Seq[_Path]
+
+    def __post_init__(self):
+        object.__setattr__(self, "inputs", tuple(self.inputs))
+
+
+async def main(args: Arguments):
+    async def unsplit(path: _Path):
+        async def splitPaths():
+            for count, _ in enumerate(_cycle((None,)), 1):
+                try:
+                    yield await _Path("{}.{:03}".format(path, count)).resolve(
+                        strict=True
+                    )
+                except FileNotFoundError:
+                    break
+
+        async with await path.open(mode="wb") as file:
+            async for splitPath in splitPaths():
+                async with await splitPath.open(mode="rb") as split:
+                    await file.write(await split.read())
+
+    await _gather(*map(unsplit, args.inputs))
+
+
+def parser(parent: _Call[..., _ArgParser] | None = None):
+    prog0 = _mods[__name__].__package__
+    prog = prog0 if prog0 else __name__
+    del prog0
+
+    parser = (_ArgParser if parent is None else parent)(
+        prog=f"python -m {prog}",
+        description="unsplit file(s)",
+        add_help=True,
+        allow_abbrev=False,
+        exit_on_error=False,
+    )
+    parser.add_argument(
+        "inputs",
+        action="store",
+        nargs=_ONE_OR_MORE,
+        type=_Path,
+        help="sequence of input(s) to read",
+    )
+
+    @_wraps(main)
+    async def invoke(args: _NS):
+        await main(Arguments(inputs=args.inputs))
+
+    parser.set_defaults(invoke=invoke)
+    return parser
+
+
+if __name__ == "__main__":
+    _basicConfig(level=_INFO)
+    entry = parser().parse_args(_argv[1:])
+    _run(entry.invoke(entry))
